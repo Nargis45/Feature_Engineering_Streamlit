367099485669d7b009f6481a933574026162a88a
Nargis Nasreen <60965420+Nargis45@users.noreply.github.com>
2024-11-07 15:17:41 +0530

    Update feature_engg.py

--- feature_engg.py
+++ feature_engg.py
@@ -1,2 +1,5 @@
 import streamlit as st
-st.write('hello')
+from streamlit_navigation_bar import st_navbar
+
+page = st_navbar(["Home", "Feature Transformation", "Feature Construction", "Featue Scaling", "Feature Extraction"])
+st.write(page)
