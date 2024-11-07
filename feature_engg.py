0e55a8a4e985b859066844e104249189fa110191
Nargis Nasreen <60965420+Nargis45@users.noreply.github.com>
2024-11-07 19:54:07 +0530

    Update feature_engg.py

--- feature_engg.py
+++ feature_engg.py
@@ -20,13 +20,15 @@ def main():
     st.subheader("Feature Engineering is the process of using domain knowledge to extract features from raw data. Those features can be used to improve the performance of machine learning algorithms.")
     st.title("Types of Feature Engineering:")
     with st.expander("1. Feature Transformation"):
-        st.success("Feature Transformation is a technique used to transform a feature/column by applying mathematical formula which is going to be useful for further analysis")
+        st.success("Feature Transformation is a technique used to transform a feature/column by applying mathematical formula which is going to be useful for further analysis.")
     with st.expander("2. Feature Construction"):
         st.info("Feature Construction is creating new feature using existing features.")
     with st.expander("3. Feature Selection"):
         st.warning("Feature Selection is selecting important features from the given features to improve model performance.")
     with st.expander("4. Feature Extraction"):
         st.error("Feature Extraction is creating completely new features out of the given features.")
+
+    st.caption("Click on the sidebar to try the practical implementation of each type")
 
 if __name__ == "__main__":
     st.set_page_config(page_title="Streamlit Gallery by Okld", page_icon="🎈", layout="wide")
