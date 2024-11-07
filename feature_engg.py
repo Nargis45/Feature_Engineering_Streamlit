ae44b8a10b042924a80dae2c201642575e0badff
Nargis Nasreen <60965420+Nargis45@users.noreply.github.com>
2024-11-07 15:30:00 +0530

    Update feature_engg.py

--- feature_engg.py
+++ feature_engg.py
@@ -1,5 +1,28 @@
 import streamlit as st
 from streamlit_navigation_bar import st_navbar
 
-page = st_navbar(["Home", "Feature Transformation", "Feature Construction", "Featue Scaling", "Feature Extraction"])
-st.write(page)
+from streamlit_gallery import apps, components
+from streamlit_gallery.utils.page import page_group
+
+def main():
+    page = page_group("p")
+
+    with st.sidebar:
+        st.title("🎈 Okld's Gallery")
+
+        with st.expander("✨ APPS", True):
+            page.item("Streamlit gallery", apps.gallery, default=True)
+
+        with st.expander("🧩 COMPONENTS", True):
+            page.item("Ace editor", components.ace_editor)
+            page.item("Disqus", components.disqus)
+            page.item("Elements⭐", components.elements)
+            page.item("Pandas profiling", components.pandas_profiling)
+            page.item("Quill editor", components.quill_editor)
+            page.item("React player", components.react_player)
+
+    page.show()
+
+if __name__ == "__main__":
+    st.set_page_config(page_title="Streamlit Gallery by Okld", page_icon="🎈", layout="wide")
+    main()
