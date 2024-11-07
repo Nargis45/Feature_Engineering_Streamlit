798e154b6e2563902d4d12196ff4580a3f19b1c2
Nargis Nasreen <60965420+Nargis45@users.noreply.github.com>
2024-11-07 19:25:42 +0530

    Update feature_engg.py

--- feature_engg.py
+++ feature_engg.py
@@ -17,7 +17,12 @@ def main():
             st.button("Feature Selection")
             st.button("Feature Extraction")
 
-    st.write("Feature Engineering is the process of using domain knowledge to extract features from raw data. Those features can be used to improve the performance of machine learning algorithms.")
+    st.subheader("Feature Engineering is the process of using domain knowledge to extract features from raw data. Those features can be used to improve the performance of machine learning algorithms.")
+    st.title("Types of Feature Engineering:")
+    st.write("1. Feature Transformation")
+    st.write("2. Feature Construction")
+    st.write("3. Feature Selection")
+    st.write("4. Feature Extraction")
 
 if __name__ == "__main__":
     st.set_page_config(page_title="Streamlit Gallery by Okld", page_icon="🎈", layout="wide")
